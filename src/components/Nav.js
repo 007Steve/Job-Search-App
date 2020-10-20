646759e9ab7a0d115462878433267c8c5813ac4a
007steve <stephen.plummer1.sp@gmail.com>
2020-10-20 16:20:43 -0400

    fixed logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,12 @@ import './Nav.css'
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import Brightness3Icon from '@material-ui/icons/Brightness3';
 import ToggleOffIcon from '@material-ui/icons/ToggleOff';
-import Logo from'src/logo.svg'
+
 function Nav() {
     return (
         <div className="nav">
             <div className="nav__header">
-                <img className="nav__logo" src={Logo}  alt=""/>
+                <h2>Dev Jobs</h2>
                 <div className="nav__icons">
                     <WbSunnyIcon className="nav__icon"/>
                     <ToggleOffIcon  className="nav__icon"/>
